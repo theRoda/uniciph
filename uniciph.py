61eaedde26d288ecc4e4bc5e3d1c02b1425afbaf
Roda McRoderson <thisreallylongname@gmail.com>
2016-02-01 02:25:37 -0800

    Make adjustment to testCaesar() function.

--- uniciph.py
+++ uniciph.py
@@ -57,7 +57,7 @@ def testCaesar(ciphertext):
 			c = ord(c)
 			decoded += chr(c + testkey if c + testkey < 90 else c - testkey)
 		print('{0} : Rot{1} Caesar').format(decoded, str(testkey))
-		checkMatch(decoded, testkey, 'Ceasar')
+		checkMatch(decoded, testkey, 'Caesar')
 	
 def testTrans(self):
 	key = 1
